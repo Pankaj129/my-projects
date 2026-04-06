17065d2ccf6e289e9e952c8dd1038742c4cde679
Pankaj129 <81667964+Pankaj129@users.noreply.github.com>
2026-04-06 23:21:41 +0530

    [-] SpringMVC | removed unused constructor

--- SpringMVC/SpingMVC_using_SpringBoot/src/main/java/com/app/pojos/User.java
+++ SpringMVC/SpingMVC_using_SpringBoot/src/main/java/com/app/pojos/User.java
@@ -41,12 +41,4 @@ public class User extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	private RoleEnum role;
 
-	public User(String firstname, String lastname) {
-		super();
-		this.firstname = firstname;
-		this.lastname = lastname;
-	}
-	
-	
-
 }
